c7593c876de102739250c7bfcdda56d4b918e12e
GitHub Action <action@github.com>
2022-06-18 16:42:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/matthewbishop/intellijconstantparameter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.matthewbishop.intellijconstantparameter.services
+
+import com.github.matthewbishop.intellijconstantparameter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/matthewbishop/intellijconstantparameter/services/MyProjectService.kt
+++ src/main/kotlin/com/github/matthewbishop/intellijconstantparameter/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.matthewbishop.intellijconstantparameter.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.matthewbishop.intellijconstantparameter.MyBundle
 
 class MyProjectService(project: Project) {
 
